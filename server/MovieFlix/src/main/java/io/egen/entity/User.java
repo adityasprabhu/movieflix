3c5d2304186b2d952b178c87068fba0ed36ced99
adityasprabhu <prabhuadityas@gmail.com>
2016-05-07 19:51:26 -0700

    feat(init): intital commit

---
+++ server/MovieFlix/src/main/java/io/egen/entity/User.java
@@ -0,0 +1,38 @@
+package io.egen.entity;
+
+import org.hibernate.annotations.GenericGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@Entity
+@Data
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@NamedQueries (
+			@NamedQuery(name="User.findByEmail", query="Select u from User u where u.email= :pEmail")
+		)
+public class User
+{
+	@Id
+	@GenericGenerator(strategy="uuid2", name="myuuid")
+	@GeneratedValue(generator="myuuid")
+	private String id;
+	private String firstName;
+	private String lastName;
+	
+	@Column(unique=true)
+	private String email;
+	public String getEmail() { return email;}
+	public void setEmail(String email) {this.email = email;}
+	
+	private String password;
+}
